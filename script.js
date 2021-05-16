9d37f5bb12a1d8907f9bb77964f46ac216f9acb2
mariangace <mariangace@gmail.com>
2021-05-16 18:10:59 -0500

    adding text editor functionality

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 /**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
- * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
- * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
+ * DONE: Update the text in the "Formatted Text" section as a user types in the textarea
+ * DONE TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
+ * DONE: Add an .underline class to "Formatted Text" when Underline button is clicked
  * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
  */
-
 
 /**
  * Update the output text as a user types in the textarea
  * HINT: Use the onkeydown function inside HTML
  */
-function updateText(){
-  // CODE GOES HERE
+function updateText() {
+  //get text from textInput
+  let text = document.getElementById('text-input').value;
+  //copy text on textOutput;
+  document.getElementById('text-output').innerHTML = text;
 }
 
 /**
@@ -21,14 +23,18 @@ function updateText(){
  * HINT: Use the classList property
  * HINT: Toggle .active class for the button
  */
-function makeBold(elem){
+function makeBold(elem) {
   //CODE GOES HERE
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('bold');
 }
 
 /**
  * Toggle the italic class for the output text
  */
-function makeItalic(elem){
+function makeItalic(elem) {
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('italic');
 }
 
 /**
@@ -37,8 +43,14 @@ function makeItalic(elem){
  * HINT: Use the classList property
  * HINT: Use contains, remove, and add functions
  */
-function makeUnderline(elem){
-    //CODE GOES HERE
+function makeUnderline(elem) {
+  elem.classList.toggle('active');
+  let formattedText = document.getElementById('text-output');
+  if (!formattedText.classList.contains('underline')) {
+    formattedText.classList.add('underline');
+  } else {
+    formattedText.classList.remove('underline');
+  }
 }
 
 /**
@@ -47,6 +59,12 @@ function makeUnderline(elem){
  * HINT: Use the style property of the element
  * HINT: Make sure to untoggle the active state for all other align buttons
  */
-function alignText(elem, alignType){
+function alignText(elem, alignType) {
   // CODE GOES HERE
+  let buttonList = document.getElementsByClassName('align');
+  for (button of buttonList) {
+    button.classList.remove('active');
+  }
+  document.getElementById('text-output').style.textAlign = alignType;
+  elem.classList.add('active');
 }
